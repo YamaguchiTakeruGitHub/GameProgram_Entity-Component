f89b3d811dd91cb7607830604b8cf3a93bc91b01
Tudo <mmikan437@gmail.com>
2025-06-11 14:50:41 +0900

    2025.6.11SchoolFinal

--- GameProgram_Entity-Component/GameProgram_Entity-Component/GameEngine/Entity/Entity.h
+++ GameProgram_Entity-Component/GameProgram_Entity-Component/GameEngine/Entity/Entity.h
@@ -2,28 +2,38 @@
 #include "../Component/Base/Component.h"
 #include "../Component/Base/Common_Component.h"
 
+/// <summary>
+/// Entityクラス:複数のComponentを動的に保持・管理するエンティティクラス
+/// </summary>
 class Entity
 {
 private:
+	//各Componentを型名文字列で管理(型名 -> Componentのポインタ)
 	std::unordered_map<std::string, std::shared_ptr<Component>> components;
 
-public:
 protected:
-
+	//任意の型Tのコンポーネントを作成・追加するテンプレート関数
+	//引数はそのままTのコンストラクタに渡す
 	template<typename T, typename... Args>
 	void AddComponent(Args&&... _args)
 	{
+		//typeid(T).name()をキーにしてコンポーネントを格納
 		components[typeid(T).name()] = std::make_shared<T>(std::forward<Args>(_args)...);
 	}
 
+	// 型 T のコンポーネントを取得するテンプレート関数
+	// 共有ポインタを dynamic_pointer_cast でキャストして返す
 	template<typename T>
 	std::shared_ptr<T> GetComponent()
 	{
+		// マップから型名に対応するコンポーネントを検索
 		auto it = components.find(typeid(T).name());
 		if (it != components.end())
 		{
+			// 共有ポインタのキャスト
 			return std::dynamic_pointer_cast<T>(it->second);
 		}
+		// 見つからなかった場合は nullptr を返す
 		return nullptr;
 	}
 
@@ -34,8 +44,6 @@ protected:
 	void DrawComponent();
 
 	void FinalComponent();
-
-
 
 };
 
